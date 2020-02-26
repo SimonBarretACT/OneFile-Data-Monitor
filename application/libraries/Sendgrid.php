a895b570798942803a6909fd0e8c259e823964c1
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-02-26 12:12:28 +0000

    Remove sendgrid

--- application/libraries/Sendgrid.php
+++ application/libraries/Sendgrid.php
@@ -16,9 +16,6 @@ class Sendgrid {
             $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
             try {
                 $response = $sendgrid->send($email);
-                print $response->statusCode() . "\n";
-                print_r($response->headers());
-                print $response->body() . "\n";
             } catch (Exception $e) {
                 echo 'Caught exception: '. $e->getMessage() ."\n";
             }
